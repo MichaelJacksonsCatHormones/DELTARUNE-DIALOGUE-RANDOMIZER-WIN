2d8346ba352dfd4ee18a8a268b897c358ce39dba
angelinaDW <159846711+MichaelJacksonsCatHormones@users.noreply.github.com>
2024-02-24 15:37:23 -0800

    Working better now

--- DELTARUNE DIALOGUE RANDOMIZER/DELTARUNE Chapter 1&2 Data/lang/randomizer.py
+++ DELTARUNE DIALOGUE RANDOMIZER/DELTARUNE Chapter 1&2 Data/lang/randomizer.py
@@ -1,5 +1,6 @@
 import json
 import random
+import re
 
 print("Randomizing dialog....")
 filename = "lang_en_ch1_old.json"
@@ -17,8 +18,42 @@ keys = list(c.keys())
 vals = list(c.values())
 
 random.shuffle(vals)
+
+# Regular expressions
+pat1 = r'\^(\d)'
+pat2 = r'\\M.'
+
+# Now apply postprocessing to ensure we r good
+for index, v in enumerate(vals):
+    og = list(c.values())[index]
+    matches = re.findall(pat1, og)
+    for match in matches:
+        v += " ^" + match + " "
+
+    if og[-2:] == "/%" and v[-2:] != "/%":
+        v += "/%"
+    
+    matches = re.findall(pat2, og)
+    for match in matches:
+        v = match + v
+    if "%%" in og and "%" not in v:
+        v += " %%"
+    elif " %" in og  and "%" not in v:
+        v += " %"
+    #print(v)
+    vals[index] = v
+        
+
+
 out = dict(zip(keys, vals))
 
 with open("lang_en_ch1.json", "w") as outF:
     json.dump(out, outF)
 print("Finished.")
+
+
+
+#TODO
+'''
+Now that basic stuff is working, we need to try and edit everything to make sure it is advanceabl
+'''
