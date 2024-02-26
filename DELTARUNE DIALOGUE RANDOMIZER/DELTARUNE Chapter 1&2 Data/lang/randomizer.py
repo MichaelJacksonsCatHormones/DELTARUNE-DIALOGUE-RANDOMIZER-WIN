2a5407854280f5f98badce215998a7103e6b200b
angelinaDW <159846711+MichaelJacksonsCatHormones@users.noreply.github.com>
2024-02-25 20:30:55 -0800

    First working implementation

--- DELTARUNE DIALOGUE RANDOMIZER/DELTARUNE Chapter 1&2 Data/lang/randomizer.py
+++ DELTARUNE DIALOGUE RANDOMIZER/DELTARUNE Chapter 1&2 Data/lang/randomizer.py
@@ -1,6 +1,15 @@
 import json
 import random
 import re
+import time 
+
+def insert(string, substring):
+    if len(string) <= 4:
+        string = string + substring
+        return string
+    random_index = random.randint(0, len(string)-2)
+    string = string[:random_index] + substring + string[random_index:]
+    return string
 
 print("Randomizing dialog....")
 filename = "lang_en_ch1_old.json"
@@ -20,28 +29,50 @@ vals = list(c.values())
 random.shuffle(vals)
 
 # Regular expressions
-pat1 = r'\^(\d)'
-pat2 = r'\\M.'
+pat1 = r'\^(\d)+'
+pat2 = r'\\[a-zA-Z].'
 
 # Now apply postprocessing to ensure we r good
 for index, v in enumerate(vals):
+    # Remove things
+    #v = re.sub(pat1, "", v);
+    v = re.sub(pat2, "", v);
+    #v = v.replace("\\", "");
+    v = v.replace("%", "");
+    v = v.replace("/", "")
+    
     og = list(c.values())[index]
     matches = re.findall(pat1, og)
-    for match in matches:
-        v += " ^" + match + " "
+    #for match in matches:
+    #    #print(match)
+    #    v += " ^1 "
 
-    if og[-2:] == "/%" and v[-2:] != "/%":
+    if  "/%" in og and '/%' not in v:
         v += "/%"
     
     matches = re.findall(pat2, og)
     for match in matches:
-        v = match + v
+        v = " " + match + " " + v
     if "%%" in og and "%" not in v:
         v += " %%"
     elif " %" in og  and "%" not in v:
         v += " %"
     #print(v)
-    vals[index] = v
+    if "//" in og and "//" not in v:
+        v = "//" + v
+    if "\\" in og and "\\" not in v:
+        v += "\\"
+    #v = v[0:len(v)-3] + og[-3:]
+    matches = re.findall(pat1, v)
+    for match in matches:
+        v = v.replace("^"+match, "^1")
+        if len(v) > len(og):
+            v = v[0:len(og)]
+    vals[index] = v + "/%"
+    #print("old: " + og)
+    #print("new: " + v)
+    #time.sleep(1)
+    
         
 
 
